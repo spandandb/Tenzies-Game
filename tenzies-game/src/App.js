f6e4f4c23efdeb0093974cb14d3f85b4463050c0
Spandan Das Barman <spandandb94@gmail.com>
2022-04-24 20:55:26 +0530

    base container created

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import React from 'react';
 
 function App() {
-  return <h1>Hello World !!! </h1>;
+  return (
+    <div className='main--container'>
+      <h1>Hello World !!!</h1>
+    </div>
+  );
 }
 
 export default App;
